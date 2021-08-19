2439aa8f14f382c18b7db697da618a6ea8f20273
Arshdeep Singh <40192673+arshharkial@users.noreply.github.com>
2021-08-19 14:07:49 +0530

    Added Collaborative Editing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+
 # import logging
 
 # logging.basicConfig(level=logging.INFO)
@@ -7,21 +8,19 @@ from flask_socketio import SocketIO, emit
 
 async_mode = None
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='public/static',
-            template_folder='public/templates')
-app.config['SECRET_KEY'] = 'secret!'
+app = Flask(__name__, static_url_path="", static_folder="public/static", template_folder="public/templates")
+app.config["SECRET_KEY"] = "secret!"
 
 # sio = SocketIO(app)
 # sio = SocketIO(app, async_mode=async_mode)
 sio = SocketIO(app, logger=True, engineio_logger=True)
 
 
-@app.route('/')
+
+@app.route("/")
 def index():
     # logger.info('serving root')
-    return render_template('index.html')
+    return render_template("index.html")
     # return render_template(
     #     'index.html', async_mode=socketio.async_mode)
 
@@ -31,12 +30,19 @@ def connect():
     print(f"{request.sid} connected")
 
 
-@sio.on('disconnect')
+@sio.on("TextUpdated")
+def update_text(message):
+    with open("text.txt", "w+") as file:
+        file.write(message)
+        emit("UpdateText", message, broadcast=True)
+
+
+@sio.on("disconnect")
 def test_disconnect():
     print(f"{request.sid} disconnected")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # sio.run(app)
     sio.run(app, host="localhost", port=8000, debug=True)
     # sio.run(app, host='0.0.0.0', port=5000) # production
--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,8 +1,19 @@
 const sio = io();
 
 sio.on('connect', () => {
+    sio.send("User has connected!!")
     console.log('connected');
 });
+
+sio.on("UpdateText", (message) => {
+    document.getElementById("TextArea").value = message;
+    console.log("Message Received");
+});
+
+function myFunction(){
+    sio.emit("TextUpdated", document.getElementById("TextArea").value);
+    console.log("Button Clicked");
+}
 
 sio.on('disconnect', () => {
     console.log('disconnected');
